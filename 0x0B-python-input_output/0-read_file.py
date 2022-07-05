c4749ce79bd8c1a25084fccae2419ce86f16fcd2
Leestan360 <stanleymayore2@gmail.com>
2022-07-05 21:04:19 +0000

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""Module 0-read_file.
-Reads from a file and prints.
-"""
+"""Read file"""
 
 
 def read_file(filename=""):
-    """Reads from filename and prints
-    its contents to stdout.
-
-    Args:
-        - filename: name of the file
-    """
-
-    with open(filename) as f:
-        read_text = f.read()
-        print(read_text, end="")
+    """""reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")

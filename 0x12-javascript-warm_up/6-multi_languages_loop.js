fabd61c1f1852b51895bb18f872e84be9e219f98
Leestan360 <stanleymayore2@gmail.com>
2022-09-06 00:17:57 +0300

    Finish project

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const s = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < s.length; i++) {
   console.log(s[i]);
-};
+}
